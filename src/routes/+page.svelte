38fece572c2609f47dc37e9c2269594829927089
sammuu <47285750+samuelpita@users.noreply.github.com>
2024-01-02 13:11:26 -0800

    Added content of first page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import imageCover from "$lib/assets/images/cover-image.jpg";
+</script>
+
+<div class="cover">
+    <img src={imageCover} alt="Samuel Pita" />
+    <div class="info">
+        <h1>Samuel Pita</h1>
+        <p>Programmer & Graphics Designer</p>
+        <p>Grade 12 Student in Whistler Secondary</p>
+    </div>
+</div>
+
+<style>
+
+</style>
