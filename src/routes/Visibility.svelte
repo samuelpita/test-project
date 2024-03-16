7288dca758a1a26813a945437aff3bd11d322948
sammuu <47285750+samuelpita@users.noreply.github.com>
2024-03-16 11:44:47 -0700

    Added Visibility and IconVideo component files

---
+++ src/routes/Visibility.svelte
@@ -0,0 +1,35 @@
+<script>
+    import IntersectionObserver from "svelte-intersection-observer";
+
+    /** @type {HTMLElement} */
+    let self;
+
+    export let style = "";
+    export let styleObserving = "";
+    export let styleNotObserving = "";
+
+    export let once = false;
+    export let threshold = 1;
+
+    /**
+     * @param {CustomEvent<IntersectionObserverEntry>} e
+     */
+    export let onObserve = (e) => {
+        self.className =
+            style + " " + (e.detail.isIntersecting ? styleObserving : styleNotObserving);
+    };
+</script>
+
+<div bind:this={self}>
+    <IntersectionObserver
+        element={self}
+        on:observe={onObserve}
+        {once}
+        {threshold}
+        let:entry
+        let:intersecting
+        let:observer
+    >
+        <slot {entry} {intersecting} {observer} />
+    </IntersectionObserver>
+</div>
