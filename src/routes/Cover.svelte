e56a3f2af8a93748f0c47271da2db6bebc93b328
Samuel Pita <samuelp13@sd48.org>
2024-04-02 14:19:21 -0700

    Added a setting that forces vertical mode for Cover component

--- src/routes/Cover.svelte
+++ src/routes/Cover.svelte
@@ -1,14 +1,35 @@
 <script>
     export let flipped = false;
+    export let forceVertical = false;
 
     export let styleContentColor =
         "bg-gradient-to-t from-black to-transparent md:bg-gradient-to-tl md:from-primary-800 md:to-secondary-700";
     export let styleContentLayout = "flex flex-col align-center justify-center px-8 py-16";
+
+    export let styleContentColorFV = "bg-gradient-to-t from-black to-transparent";
+    export let styleContentLayoutFV = "flex flex-col align-center justify-center px-8 py-16";
+
     export let styleGraphic = "";
 </script>
 
 <div class="size-full relative md:grid md:grid-cols-2 md:grid-rows-1">
-    {#if flipped}
+    {#if forceVertical}
+        {#if flipped}
+            <div class="size-full relative {styleGraphic}">
+                <slot name="graphic" />
+            </div>
+            <div class="w-full absolute bottom-0 {styleContentLayoutFV} {styleContentColorFV}">
+                <slot name="content" />
+            </div>
+        {:else}
+            <div class="z-10 w-full absolute bottom-0 {styleContentLayoutFV} {styleContentColorFV}">
+                <slot name="content" />
+            </div>
+            <div class="z-0 size-full relative {styleGraphic}">
+                <slot name="graphic" />
+            </div>
+        {/if}
+    {:else if flipped}
         <div class="size-full relative {styleGraphic}">
             <slot name="graphic" />
         </div>
