d13b0fbb76dbd5cd574c3020952393285526e3d6
Samuel Pita <samuelp13@sd48.org>
2024-04-05 11:10:19 -0700

    Added homepage file, and minor changes to app.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
 <script>
+    import { determineInnerWidthType } from "$lib";
+
     import Card from "./Card.svelte";
     import CardImage from "./Card_Image.svelte";
     import Cover from "./Cover.svelte";
+    import IconVideo from "./IconVideo.svelte";
     import Section from "./Section.svelte";
+    import Visibility from "./Visibility.svelte";
+
+    $: innerWidth = 0;
 </script>
+
+<svelte:window bind:innerWidth />
+
+<div class="w-full h-svh">
+    <Cover styleGraphic="border-x-8 border-t-8 md:border-b-8 border-white">
+        <!--  -->
+    </Cover>
+</div>
