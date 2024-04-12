263195eb03aa17b5dd85fc2ae0555c7e29bc9024
Samuel Pita <samuelp13@sd48.org>
2024-04-12 11:15:08 -0700

    Added portfolio page

--- src/routes/Section.svelte
+++ src/routes/Section.svelte
@@ -2,14 +2,14 @@
     export let icon = "";
     export let iconAlt = "";
 
-    export let styleHeader = "items-center mb-4";
+    export let styleHeader = "";
     export let styleIconDiv = "size-24 mr-4";
     export let styleSectionColor = "";
     export let styleSectionLayout = "flex-col max-w-[40rem] xl:max-w-[56rem] min-w-0 mx-auto p-8";
 </script>
 
 <section class="flex {styleSectionLayout} {styleSectionColor}">
-    <header class="flex {styleHeader}">
+    <header class="flex items-center mb-4 {styleHeader}">
         {#if icon != ""}
             <div class={styleIconDiv}>
                 <img src={icon} alt={iconAlt} class="size-full object-cover" />
---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { determineInnerWidthType } from "$lib";
+
+    import Card from "../Card.svelte";
+    import CardImage from "../Card_Image.svelte";
+    import Cover from "../Cover.svelte";
+    import IconVideo from "../IconVideo.svelte";
+    import Section from "../Section.svelte";
+    import Visibility from "../Visibility.svelte";
+
+    $: innerWidth = 0;
+</script>
+
+<svelte:window bind:innerWidth />
+
+<Section icon="../favicon.png">
+    <h2 slot="header" class="font-sans">My Family</h2>
+    <svelte:fragment slot="content">
+        <!--  -->
+    </svelte:fragment>
+</Section>
+
+<Section icon="../favicon.png" styleHeader="flex-row-reverse">
+    <h2 slot="header" class="font-sans">Where I've been</h2>
+    <svelte:fragment slot="content">
+        <!--  -->
+    </svelte:fragment>
+</Section>
+
+<Section icon="../favicon.png">
+    <h2 slot="header" class="font-sans">Where I'm going</h2>
+    <svelte:fragment slot="content">
+        <!--  -->
+    </svelte:fragment>
+</Section>
