62bafd10c1d21fba17ecc436c20f47c8633ac2be
sammuu <47285750+samuelpita@users.noreply.github.com>
2024-04-13 10:47:33 -0700

    Changes in app.css and Section component

--- src/routes/Section.svelte
+++ src/routes/Section.svelte
@@ -5,10 +5,11 @@
     export let styleHeader = "";
     export let styleIconDiv = "size-24 mr-4";
     export let styleSectionColor = "";
-    export let styleSectionLayout = "flex-col max-w-[40rem] xl:max-w-[56rem] min-w-0 mx-auto p-8";
+    export let styleSectionLayout = "flex-col min-w-0 mx-auto p-8";
+    export let styleSectionSize = "max-w-[56rem]";
 </script>
 
-<section class="flex {styleSectionLayout} {styleSectionColor}">
+<section class="flex {styleSectionSize} {styleSectionLayout} {styleSectionColor}">
     <header class="flex items-center mb-4 {styleHeader}">
         {#if icon != ""}
             <div class={styleIconDiv}>
