08e57ddcb3b357d518f8a7cbf51a8bad3628f07c
sammuu <47285750+samuelpita@users.noreply.github.com>
2024-04-17 18:25:12 -0700

    Added functionality to MouseDetect

---
+++ src/routes/MouseDetect.svelte
@@ -0,0 +1,35 @@
+<script>
+    let hover = false;
+
+    /** @type {HTMLElement} */
+    let self;
+
+    export let onHoverEnter = () => {};
+    export let onHoverLeave = () => {};
+
+    export let role = "tooltip";
+    export let style = "";
+</script>
+
+<div
+    bind:this={self}
+    class={style}
+    {role}
+    on:click={() => {
+        hover = !hover;
+        if (hover) onHoverEnter();
+        else onHoverLeave();
+    }}
+    on:mouseenter={() => {
+        if (hover) return;
+        hover = true;
+        onHoverEnter();
+    }}
+    on:mouseleave={() => {
+        if (!hover) return;
+        hover = false;
+        onHoverLeave();
+    }}
+>
+    <slot {hover} {self} />
+</div>
