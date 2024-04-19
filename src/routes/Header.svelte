e3a7adeffa80b5476821fc712fc28dac2c88cedc
Samuel Pita <samuelp13@sd48.org>
2024-04-19 10:50:46 -0700

    Added functionality to Header

---
+++ src/routes/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let flipped = false;
+
+    export let icon = "./favicon.png";
+    export let iconAlt = "";
+
+    export let stylePadding = "p-4";
+    export let styleSize = "max-w-[56rem]";
+</script>
+
+{#if flipped}
+    <header class="flex flex-row-reverse items-center mx-auto {stylePadding} {styleSize}">
+        <div class="size-32 ml-4">
+            <img src={icon} alt={iconAlt} class="size-full object-cover" />
+        </div>
+        <slot />
+    </header>
+{:else}
+    <header class="flex items-center mx-auto {stylePadding} {styleSize}">
+        <div class="size-32 mr-4">
+            <img src={icon} alt={iconAlt} class="size-full object-cover" />
+        </div>
+        <slot />
+    </header>
+{/if}
