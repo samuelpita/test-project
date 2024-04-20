63a31cbe9330054479e7a659cdbdd610c9bd414d
sammuu <47285750+samuelpita@users.noreply.github.com>
2024-04-20 10:32:45 -0700

    Added a button container mode for Hover Detection component

--- src/routes/HoverDetect.svelte
+++ src/routes/HoverDetect.svelte
@@ -4,6 +4,8 @@
     /** @type {HTMLElement} */
     let self;
 
+    export let buttonMode = true;
+
     export let onHoverEnter = () => {};
     export let onHoverLeave = () => {};
 
@@ -11,25 +13,50 @@
     export let style = "";
 </script>
 
-<div
-    bind:this={self}
-    class={style}
-    {role}
-    on:click={() => {
-        hover = !hover;
-        if (hover) onHoverEnter();
-        else onHoverLeave();
-    }}
-    on:mouseenter={() => {
-        if (hover) return;
-        hover = true;
-        onHoverEnter();
-    }}
-    on:mouseleave={() => {
-        if (!hover) return;
-        hover = false;
-        onHoverLeave();
-    }}
->
-    <slot {hover} {self} />
-</div>
+{#if buttonMode}
+    <button
+        bind:this={self}
+        class="min-w-0 min-h-0 overflow-hidden relative {style}"
+        {role}
+        on:click={() => {
+            hover = !hover;
+            if (hover) onHoverEnter();
+            else onHoverLeave();
+        }}
+        on:mouseenter={() => {
+            if (hover) return;
+            hover = true;
+            onHoverEnter();
+        }}
+        on:mouseleave={() => {
+            if (!hover) return;
+            hover = false;
+            onHoverLeave();
+        }}
+    >
+        <slot {hover} {self} />
+    </button>
+{:else}
+    <div
+        bind:this={self}
+        class={style}
+        {role}
+        on:click={() => {
+            hover = !hover;
+            if (hover) onHoverEnter();
+            else onHoverLeave();
+        }}
+        on:mouseenter={() => {
+            if (hover) return;
+            hover = true;
+            onHoverEnter();
+        }}
+        on:mouseleave={() => {
+            if (!hover) return;
+            hover = false;
+            onHoverLeave();
+        }}
+    >
+        <slot {hover} {self} />
+    </div>
+{/if}
