c520dd4c6c223a8a4bb2d307b40d116effdae7bf
sammuu <47285750+samuelpita@users.noreply.github.com>
2024-04-19 22:22:34 -0700

    Added basic functionality to Nav

---
+++ src/routes/Nav.svelte
@@ -0,0 +1,39 @@
+<script>
+    let isOpen = false;
+
+    /** @type {{ href: string, name: string }[]} */
+    export let links = [
+        { href: "#header1", name: "Header 1" },
+        { href: "#header2", name: "Header 2" },
+        { href: "#header3", name: "Header 3" },
+    ];
+</script>
+
+<div>
+    {#if isOpen}
+        <button
+            on:click={() => {
+                isOpen = false;
+            }}
+        >
+            <p class="font-sans">Back</p>
+        </button>
+        <nav>
+            <ol>
+                {#each links as link}
+                    <li>
+                        <a href={link.href}>{link.name}</a>
+                    </li>
+                {/each}
+            </ol>
+        </nav>
+    {:else}
+        <button
+            on:click={() => {
+                isOpen = true;
+            }}
+        >
+            <p class="font-sans">Open Nav</p>
+        </button>
+    {/if}
+</div>
