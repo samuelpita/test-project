5857fea44a8bfba389b9ba3c2e02662229958138
Samuel Pita <samuelp13@sd48.org>
2024-05-24 11:38:08 -0700

    Added Frame & ImageGrid components

---
+++ src/lib/components/Frame.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { glob } from "./Global.svelte";
+
+    export let layout = "";
+    export let margin = "";
+    export let maxHeight = "";
+    export let maxWidth = glob.width.main;
+    export let padding = "";
+    export let sizing = "w-full";
+</script>
+
+<div class="{layout} {margin} {maxHeight} {maxWidth} {padding} {sizing}">
+    <slot />
+</div>
---
+++ src/lib/components/ImageGrid.svelte
@@ -0,0 +1,23 @@
+<script>
+    import ImageTooltip from "./ImageTooltip.svelte";
+    import { glob } from "./Global.svelte";
+
+    export let border = true;
+    export let cols = "grid-cols-2";
+    export let gap = true;
+    export let margin = glob.margin.main.b;
+    export let round = "rounded-lg";
+
+    let DEFAULT = "w-full overflow-hidden grid auto-rows-fr";
+    if (border) DEFAULT += " bg-white border-white " + glob.borderWidth.all;
+    if (gap) DEFAULT += " " + glob.gap.main;
+</script>
+
+<div class="{DEFAULT} {cols} {margin} {round}">
+    <slot>
+        <ImageTooltip image="./home-other-0.jpeg" imageAlt="Lorem ipsum dolor sit" />
+        <ImageTooltip image="./home-other-0.jpeg" imageAlt="Lorem ipsum dolor sit" />
+        <ImageTooltip image="./home-other-0.jpeg" imageAlt="Lorem ipsum dolor sit" />
+        <ImageTooltip image="./home-other-0.jpeg" imageAlt="Lorem ipsum dolor sit" />
+    </slot>
+</div>
