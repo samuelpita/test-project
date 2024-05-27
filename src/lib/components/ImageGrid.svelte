1c3861a0366119429a4be23277523605375021f8
Samuel Pita <samuelp13@sd48.org>
2024-05-27 10:50:17 -0700

    Updated ImageGrid

--- src/lib/components/ImageGrid.svelte
+++ src/lib/components/ImageGrid.svelte
@@ -7,13 +7,14 @@
     export let gap = true;
     export let margin = glob.margin.main.b;
     export let round = "rounded-lg";
+    export let rows = "auto-rows-fr";
 
-    let DEFAULT = "w-full overflow-hidden grid auto-rows-fr";
+    let DEFAULT = "w-full overflow-hidden grid";
     if (border) DEFAULT += " bg-white border-white " + glob.borderWidth.all;
     if (gap) DEFAULT += " " + glob.gap.main;
 </script>
 
-<div class="{DEFAULT} {cols} {margin} {round}">
+<div class="{DEFAULT} {cols} {margin} {round} {rows}">
     <slot>
         <ImageTooltip image="./home-other-0.jpeg" imageAlt="Lorem ipsum dolor sit" />
         <ImageTooltip image="./home-other-0.jpeg" imageAlt="Lorem ipsum dolor sit" />
