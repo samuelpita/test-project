d03dbe08b70fe1eebd9325297cbc6c4500458f72
Samuel Pita <samuelp13@sd48.org>
2024-05-30 12:40:34 -0700

    Updated Frame component

--- src/lib/components/Frame.svelte
+++ src/lib/components/Frame.svelte
@@ -2,13 +2,16 @@
     import { glob } from "./Global.svelte";
 
     export let layout = "";
-    export let margin = "";
+    export let margin = glob.margin.main.b;
     export let maxHeight = "";
     export let maxWidth = glob.width.main;
-    export let padding = "";
+    export let innerPadding = "";
+    export let outerPadding = glob.padding.main.x;
     export let sizing = "w-full";
 </script>
 
-<div class="{layout} {margin} {maxHeight} {maxWidth} {padding} {sizing}">
-    <slot />
+<div class="{margin} {outerPadding}">
+    <div class="{layout} {maxHeight} {maxWidth} {innerPadding} {sizing}">
+        <slot />
+    </div>
 </div>
