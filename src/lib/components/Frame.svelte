eb1772c580908291e23b7d0fbeae5b68012df19f
Samuel Pita <samuelp13@sd48.org>
2024-05-31 11:33:05 -0700

    Updated Frame component

--- src/lib/components/Frame.svelte
+++ src/lib/components/Frame.svelte
@@ -3,13 +3,10 @@
     export let margin = "m-main-b";
     export let maxHeight = "";
     export let maxWidth = "max-w-main";
-    export let innerPadding = "";
-    export let outerPadding = "p-main-x";
+    export let padding = "p-main-x";
     export let sizing = "w-full";
 </script>
 
-<div class="{margin} {outerPadding}">
-    <div class="{layout} {maxHeight} {maxWidth} {innerPadding} {sizing}">
-        <slot />
-    </div>
+<div class="{margin} {layout} {maxHeight} {maxWidth} {padding} {sizing}">
+    <slot />
 </div>
