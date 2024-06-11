d48b75f590651f1b6c02fd45a3db680e0d30ee6d
Samuel Pita <samuelp13@sd48.org>
2024-06-11 14:35:40 -0700

    Restored necessary files from main branch

---
+++ src/lib/scripts/functions.js
@@ -0,0 +1,29 @@
+// functions.js
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} alpha
+ */
+export function lerp(a, b, alpha) {
+    return a + alpha * (b - a);
+}
+
+/**
+ * @param {string} type
+ */
+export function screenTypeValue(type) {
+    switch (type) {
+        case "2xl":
+            return 1536;
+        case "xl":
+            return 1280;
+        case "lg":
+            return 1024;
+        case "md":
+            return 768;
+        case "sm":
+            return 640;
+    }
+    return 0;
+}
